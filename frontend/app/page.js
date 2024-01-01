efffd228d0b97919e28ee809d3b15ed8afa3bb6b
gdziewon <116833445+gdziewon@users.noreply.github.com>
2024-01-02 00:58:13 +0100

    Add auth and image upload - front
    Added files:
     - NextAuthProvider.js which is used as wraper due to problems with
       SessionProvider wrapper
     - [...nextauth].js which contants configuration for authentication with
       NextAuth
     - /app/gallery/upload/pages.js contains the page for uploading images,
       user can select multiple images and they are sent to our
       /process-images endpoint
     - middleware.ts with further configuration of auth - upload images page
       is blocked by matcher
    
    Added Authentication using NextAuth library with query to custom API -
    out /token endpoint. In Navbar there is now button 'Log in' which
    changes to Log out after successful authorization. The token is stored
    using JWT.
    
    Updated app.py with CORS Middleware so our endpoints accept and
    requests.

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,4 +1,8 @@
+import { connectToDatabase } from "@/pages/api/connectMongo";
+
 export default function Home() {
+    connectToDatabase();
+
     return (
         <main className="container mx-auto p-4">
             <h1 className="text-4xl font-bold mb-4">Welcome to Home Page</h1>
