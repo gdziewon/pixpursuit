5a316389299ebdd382d00d320182dfb940d95995
Michał Mistarz <michal.mistarz02@gmail.com>
2024-03-02 19:36:07 +0100

    Adds custom windows
        Frontend:
        -added custom windows for removint selected items
        and adding tags for selected items

--- frontend/utils/ConfirmDialog.js
+++ frontend/utils/ConfirmDialog.js
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from 'react';
+import '@/styles/design_styles.css';
 
 const ConfirmDialog = ({ isOpen, onConfirm, onCancel }) => {
     const [error, setError] = useState(null);
@@ -10,32 +11,35 @@ const ConfirmDialog = ({ isOpen, onConfirm, onCancel }) => {
             return null;
         }
         return (
-            <div className="fixed z-10 inset-0 overflow-y-auto">
+            <div className="fixed z-10 inset-0 overflow-y-auto flex items-center justify-center">
                 <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <div className="fixed inset-0 transition-opacity" aria-hidden="true">
                         <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
                     </div>
-                    <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-                    <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                        <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <span className="hidden sm:inline-block sm:align-middle sm:h-screen"
+                          aria-hidden="true">&#8203;</span>
+                    <div
+                        className="inline-block align-bottom bg-gray-800 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                        <div className="bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                             <div className="sm:flex sm:items-start">
                                 <div className="mt-3 text-center sm:mt-0 sm:text-center">
-                                    <h3 className="text-lg leading-6 font-medium text-gray-900 text-center" id="modal-title">
+                                    <h3 className="text-lg text-white leading-6 font-medium  text-center"
+                                        id="modal-title">
                                         Are you sure you want to delete the selected items?
                                     </h3>
                                 </div>
                             </div>
                         </div>
-                        <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                        <div className="bg-gray-800 px-4 py-3 sm:px-6 sm:flex button-container">
                             <button type="button"
-                                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm mr-2"
-                                    onClick={onConfirm}>
-                                Delete
-                            </button>
-                            <button type="button"
-                                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+                                    className="mt-3 w-auto inline-flex rounded-full shadow-sm px-4 py-1.5 bg-white text-sm font-medium text-gray-800 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:text-sm mr-2 ml-4"
                                     onClick={onCancel}>
                                 Cancel
+                            </button>
+                            <button type="button"
+                                    className="mt-3 w-auto inline-flex rounded-full shadow-sm px-4 py-1.5 delete-button text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:text-sm mr-2 ml-4"
+                                    onClick={onConfirm}>
+                                Delete
                             </button>
                         </div>
                     </div>
--- frontend/utils/TagModal.js
+++ frontend/utils/TagModal.js
@@ -1,6 +1,7 @@
 "use client"
 
 import React, { useState } from 'react';
+import '@/styles/design_styles.css';
 
 const TagModal = ({ isOpen, onSubmit, onCancel, tagInput, handleTagInputChange }) => {
     const [error, setError] = useState(null);
@@ -11,17 +12,17 @@ const TagModal = ({ isOpen, onSubmit, onCancel, tagInput, handleTagInputChange }
         }
 
         return (
-            <div className="fixed z-10 inset-0 overflow-y-auto">
+            <div className="fixed z-10 inset-0 overflow-y-auto flex items-center justify-center">
                 <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <div className="fixed inset-0 transition-opacity" aria-hidden="true">
                         <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
                     </div>
                     <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-                    <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                        <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="inline-block align-bottom bg-gray-800 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                        <div className="bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                             <div className="sm:flex sm:items-start">
-                                <div className="mt-3 text-center sm:mt-0 sm:text-left">
-                                    <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                                <div className="mt-3 text-left sm:mt-0 sm:text-left">
+                                    <h3 className="text-lg leading-6 font-medium text-white" id="modal-title">
                                         Tags to add:
                                     </h3>
                                     <div className="mt-2">
@@ -29,21 +30,24 @@ const TagModal = ({ isOpen, onSubmit, onCancel, tagInput, handleTagInputChange }
                                             value={tagInput}
                                             onChange={handleTagInputChange}
                                             type="text"
-                                            style={{width: '400px', padding: '10px'}}
-                                            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block sm:text-sm border-gray-300 rounded-md text-black text-lg"
+                                            style={{width: '400px', padding: '10px', borderBottom: '1px solid white'}}
+                                            className="shadow-sm block sm:text-sm  text-whtie text-lg bg-gray-800 no-border-on-focus"
                                             placeholder="Enter tags, separated by commas"
                                         />
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                        <div className="bg-gray-800 px-4 py-3 sm:px-6 sm:flex button-container">
                             <button type="button"
-                                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm mr-2" onClick={onSubmit}>
-                                Accept
-                            </button>
-                            <button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm mr-2" onClick={onCancel}>
+                                    className="mt-3 w-auto inline-flex rounded-full shadow-sm px-4 py-1.5 bg-white text-sm font-medium text-gray-800 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:text-sm mr-2 ml-4"
+                                    onClick={onCancel}>
                                 Cancel
+                            </button>
+                            <button type="button"
+                                    className="mt-3 w-auto inline-flex rounded-full shadow-sm px-4 py-1.5 accept-button text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:text-sm mr-2 ml-4"
+                                    onClick={onSubmit}>
+                                Accept
                             </button>
                         </div>
                     </div>
